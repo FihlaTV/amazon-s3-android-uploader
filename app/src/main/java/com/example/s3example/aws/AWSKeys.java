970a18915c143a9cfbc9b92a0e252919336ae716
FihlaTV <fihlatv@gmail.com>
2020-10-14 02:49:19 +0200

    addded pool id added new name changed s3 bucket signed the apk

--- app/src/main/java/com/example/s3example/aws/AWSKeys.java
+++ app/src/main/java/com/example/s3example/aws/AWSKeys.java
@@ -3,8 +3,8 @@ package com.example.s3example.aws;
 import com.amazonaws.regions.Regions;
 
 public class AWSKeys {
-    protected static final String COGNITO_POOL_ID = "YOUR-POOL-ID-HERE";
-    protected static final Regions MY_REGION = Regions.AP_SOUTHEAST_1; /*Change Region Here*/
-    public static final String BUCKET_NAME = "YOUR-BUCKET_NAME-HERE";
+    protected static final String COGNITO_POOL_ID = "eu-west-2:aba364dc-999e-4b40-9cb4-09be4e119246";
+    protected static final Regions MY_REGION = Regions.EU_WEST_2; /*Change Region Here*/
+    public static final String BUCKET_NAME = "minderz-data";
 
 }
